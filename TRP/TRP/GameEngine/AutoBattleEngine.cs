395342861d709c4c28e3b4298880c0476372d341
Vincent Schlappi <schlapp1@seattleu.edu>
2019-02-23 08:25:43 -0800

    Adding autobattleengine start.

---
+++ TRP/TRP/GameEngine/AutoBattleEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using TRP.Models;
+
+
+namespace TRP.GameEngine
+{
+    class AutoBattleEngine
+    {
+
+        public BattleEngine BattleEngine = new BattleEngine();
+
+        public bool RunAutoBattle()
+        {
+
+            return false;
+        }
+    }
+}
